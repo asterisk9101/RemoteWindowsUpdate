7cd94a7d225b6bcdb93b70f8777c9284b17a75ba
asterisk9101 <tm11b028@gmail.com>
2018-09-02 02:51:55 +0900

    first commit

---
+++ RemoteWindowsUpdate.psm1
@@ -0,0 +1,266 @@
+# 概要
+# 
+# リモートホストで Windows Update を実行する
+# 
+# 
+# 詳細
+# 
+# RemoteWindowsUpdate.psm1 は、リモートホストで Windows Update を実行するために 
+# Invoke-WindowsUpdate コマンドレットを利用可能にする。
+# 
+# Invoke-WindowsUpdate コマンドレットは、あるドメインに所属するホストから同じドメイン
+# に所属するリモートホストに対して実行されることを想定している。そのため、認証情報を受け
+# 取る Credential パラメータを持たないが、Windows Update を実行するために管理者
+# （Domain Admins 相当）の権限を必要とする。
+# 
+# Invoke-WindowsUpdate は、Windows Update を実行するスクリプトを対象のリモートホスト
+# に設置する。特に変更しなければリモートホストに C:\Invoke-RemoteTask.ps1 が作成される。
+# 
+# 続いて、対象のリモートホストにスクリプトを実行するタスクをタスクスケジューラのルートに
+# 作成し、実行する。スクリプトを直接実行しないのは Windows Update がリモート実行できな
+# いための回避策である。
+# 
+# タスクが完了すると、タスクとスクリプトは削除され、対象のリモートホストに痕跡は残らない。
+# 
+# AutoReboot スイッチが有効な場合、Windows Update が完了すると、再起動要否が確認され、
+# 必要であれば対象のリモートホストは再起動される。
+# 
+# 最後に、利用可能な Update の有無が確認される。利用可能な Update が無い場合、リモート
+# ホストに適用されている Update の一覧がローカルホストのカレントディレクトリに出力される。
+# ジョブとして実行されている場合は、完了ステータスが、Completed になる。
+# 利用可能な Update が残っている場合、ローカルホストのカレントディレクトリにその一覧が
+# 出力され、アップデート未完了の例外が発生する。従って、ジョブとして実行されている場合は、
+# 完了ステータスが Failed になる。
+# 
+# 完了ステータスが Failed になった場合は、単純に Invoke-WindowsUpdate を再実行するだけ
+# で、たいてい解決する。問題がある場合はトラブルシューティングの項目を参照のこと。
+# 
+# 
+# 使い方
+# 
+# Import-Module -Verbose -Force RemoteWindowsUpdate.psm1
+# Invoke-WindowsUpdate -AsJob -AutoReboot -ComputerName {対象ホスト名}
+# 
+# 
+# 利用要件
+# 
+# Windows Server 2012R2 以上
+# もしくは PowerShell 3.0 以上で WinRM が利用可能であること
+# 
+# 
+# トラブルシューティング
+# 
+# Invoke-WindowsUpdate を実行すると、すぐに異常終了してしまう場合、同名のタスクが
+# 実行中である可能性がある。先行しているタスクが終了するのを待ってから再実行する。
+# 
+# 前回のスクリプトが異常終了し、一時ファイルが残存した場合も同じ問題が発生する。
+# その場合は、リモートホストに作成された C:\Invoke-RemoteTask.ps1 を削除することで解消する。
+# 
+
+# PowerShell のバックグラウンドジョブは、元のスクリプトとは別プロセスになるため
+# バックグラウンドジョブの内部で必要な関数は、全て $InitScript にまとめている。
+$InitScript = {
+    # Windows Update(Microsoft Update)　を実行する
+    Function Install-WindowsUpdate {
+        Param(
+            [Parameter(Position=1,Mandatory=$False)]
+            [switch]$ListOnly
+        )
+        $ErrorActionPreference = "Stop"
+
+        $dir = mkdir -Force C:\temp
+        Set-Location $dir
+
+        # アップデートを検索する
+        $updateSession = New-Object -ComObject Microsoft.Update.Session
+        $searcher = $updateSession.CreateUpdateSearcher()
+
+        # 参照するサーバはグループポリシーで設定済みの場合は、デフォルトを選択する。
+        $searcher.ServerSelection = 0
+
+        # 未インストールの項目を全て検索する。
+        $searchResult = $searcher.search("IsInstalled=0")
+
+        if ($ListOnly) {
+            # ListOnly スイッチが True
+            return $searchResult
+        }
+
+        if ($searchResult.Updates.Count -eq 0) {
+            # Update なし
+            return
+        }
+
+        # EULA を承諾してダウンロードリストに追加する
+        # WSUSで承認済みのため承諾は自動でOK
+        $updatesToDownload = New-Object -ComObject Microsoft.Update.UpdateColl
+        $searchResult.Updates | ForEach-Object {
+            $update = $_
+            if ($update.InstallationBehavior.CanRequestUserInput) {
+                # 手作業が必要
+                return
+            }
+            if (-not $_.EulaAccepted) {
+                # EULA を承諾する
+                $_.AcceptEula()
+            }
+            $updatesToDownload.add($update) | Out-Null
+        }
+
+        if ($updatesToDownload.Count -eq 0) {
+            # 手作業でEULAの承諾が必要
+            throw "Could not accept the EULA"
+        }
+
+        # ダウンロードを開始
+        $downloader = $updateSession.CreateUpdateDownloader()
+        $downloader.Updates = $updatesToDownload
+        $downloader.Download()
+
+        # ダウンロードが完了した Update をインストールリストに追加する
+        $updatesToInstall = New-Object -ComObject Microsoft.Update.UpdateColl
+        $searchResult.Updates | Where-Object {
+            $_.IsDownloaded
+        } | ForEach-Object {
+            $updatesToInstall.add($_) | Out-Null
+        }
+
+        if ($updatesToInstall.Count -eq 0) {
+            # ダウンロード失敗
+            throw "Update Download Faild"
+        }
+
+        # "インストール開始"
+        $installer = $updateSession.CreateUpdateInstaller()
+        $installer.Updates = $updatesToInstall
+        $installationResult = $installer.Install()
+
+        if ($installationResult.ResultCode -ne 2) {
+            # "インストール失敗"
+            throw "Update Install Faild"
+        }
+    }
+
+    # ps1 スクリプトをリモートホストに転送してタスクとして実行する。
+    # Windows Update（スクリプト）は、リモート実行できないが、
+    # タスクを使って間接的に実行するとローカルとして扱われるため実行可能となる
+    Function Invoke-RemoteTask {
+        Param(
+            [Parameter(Position=1,Mandatory=$True)]
+            [String]$ComputerName,
+
+            [Parameter(Position=2,Mandatory=$True)]
+            [ScriptBlock]$ScriptBlock,
+
+            [Parameter(Position=3,Mandatory=$False)]
+            [string]$TaskName = "Invoke-RemoteTask"
+        )
+        $ErrorActionPreference = "Stop"
+
+        # スクリプトの転送
+        $FilePath = "\\$ComputerName\C$\$TaskName.ps1"
+        if (Test-Path $FilePath) {
+            # 既に同名のファイルが存在する場合は例外を投げる
+            throw "Already File Exists"
+        }
+        $ScriptBlock.ToString() | Out-File -Encoding UTF8 -FilePath $FilePath -NoClobber
+
+        # CIM セッションの作成
+        $cim = New-CimSession -ComputerName $ComputerName
+
+        # 既存のタスクが実行中の場合は例外を投げる
+        $Task = Get-ScheduledTask -CimSession $cim -TaskName $TaskName -TaskPath \ -ErrorAction Silent
+        if ($Task.State -eq "Running") {
+            throw "Task is Running."
+        }
+
+        # タスクを登録（既存のタスクは上書き）
+        $ScriptName = "C:\$TaskName.ps1"
+        $Action = New-ScheduledTaskAction -Execute "PowerShell.exe -File $ScriptName"
+        Register-ScheduledTask -CimSession $cim -TaskName $TaskName -TaskPath \ -Action $Action -User "SYSTEM" -Force -Runlevel 1 | Out-Null
+        
+        # タスクを実行
+        Start-ScheduledTask -CimSession $cim -TaskName $TaskName -TaskPath \
+
+        # タスクの完了まで待機
+        $Task = Get-ScheduledTask -CimSession $cim -TaskName $TaskName -TaskPath \
+        while ($Task.State -eq "Running") {
+            Start-Sleep -Seconds 10
+            $Task = Get-ScheduledTask -CimSession $cim -TaskName $TaskName -TaskPath \
+        }
+
+        # タスクの削除
+        Unregister-ScheduledTask -CimSession $cim -TaskName $TaskName -TaskPath \ -Confirm:$False
+
+        # CIM セッションの削除
+        Remove-CimSession $cim
+
+        # スクリプトの削除
+        Remove-Item -Force $FilePath
+    }
+}
+
+$RemoteWindowsUpdate = {
+    Param(
+        [Parameter(Position=1,Mandatory=$True)]
+        [string]$ComputerName,
+
+        [Parameter(Position=2,Mandatory=$False)]
+        [switch]$AutoReboot,
+
+        [Parameter(Position=3,Mandatory=$False)]
+        [switch]$ListOnly
+    )
+
+    $ErrorActionPreference = "Stop"
+
+    # スクリプト(タスク)をリモートで実行
+    Invoke-RemoteTask -ComputerName $ComputerName -ScriptBlock ${function:Install-WindowsUpdate}
+
+    # 再起動要否を取得
+    $RebootRequired = Invoke-Command $ComputerName -ScriptBlock {
+        Test-Path "HKLM:SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired"
+    }
+
+    # 必要があれば再起動する
+    if ($RebootRequired -and $AutoReboot) {
+        Restart-Computer $ComputerName -Wait -Force
+    }
+
+    # 追加のアップデートがあれば例外を投げる
+    $Available = Invoke-Command -ComputerName $ComputerName -ScriptBlock ${function:Install-WindowsUpdate} -ArgumentList $True
+    
+    $FilePath = "{0}_{1}.txt" -f $ComputerName,(Get-Date -f "yyyyMMdd")
+    if ($Available.Updates.Count -ne 0) {
+        $updates = $Available.Updates | Select-Object @{L="KB";E={$_.KBArticleIds -join ","}},Title,LastDeploymentChangeTime
+        $updates | ConvertTo-Csv -NTI | Out-File -Encoding Default -FilePath $FilePath -Force
+        throw "$ComputerName Update Uncomplete"
+    } else {
+        $hosfix = Get-HotFix -ComputerName $ComputerName | Sort-Object InstalledOn
+        $hotfix | ConvertTo-Csv -NTI | Out-File -Encoding Default -FilePath $FilePath -Force
+        Write-OutPut "$ComputerName Update Complete"
+    }
+}
+
+Function Invoke-WindowsUpdate {
+    Param(
+        [Parameter(Position=1,Mandatory=$True)]
+        [string]$ComputerName,
+
+        [Parameter(Position=2,Mandatory=$False)]
+        [switch]$AutoReboot,
+
+        [Parameter(Position=3,Mandatory=$False)]
+        [switch]$AsJob,
+
+        [Parameter(Position=4,Mandatory=$False)]
+        [switch]$ListOnly
+    )
+    if ($AsJob) {
+        Start-Job -Name $ComputerName -InitializationScript $InitScript -ScriptBlock $RemoteWindowsUpdate -ArgumentList $ComputerName,$AutoReboot,$ListOnly | Out-Null
+    } else {
+        Start-Job -Name $ComputerName -InitializationScript $InitScript -ScriptBlock $RemoteWindowsUpdate -ArgumentList $ComputerName,$AutoReboot,$ListOnly | Receive-Job -AutoRemoveJob -Wait
+    }
+}
+
+Export-ModuleMember -Function "Invoke-WindowsUpdate"
